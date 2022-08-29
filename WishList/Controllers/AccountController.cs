538f193736dc27e521ed7fe2d4c0a95627518dc1
ahmed Abd El-Halim <a.halim2290@gmail.com>
2022-08-29 21:06:38 +0200

    add registration

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Models;
+using WishList.Models.AccountViewModles;
 
 namespace WishList.Controllers
 {
@@ -15,11 +16,46 @@ namespace WishList.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager)
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
         }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> RegisterAsync(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerViewModel);
+            }
+
+            var result = await _userManager.CreateAsync(new ApplicationUser
+            {
+                Email = registerViewModel.Email,
+                PasswordHash = registerViewModel.Password,
+                UserName = registerViewModel.Email
+            });
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("Password", error.Description);
+                }
+                return View(registerViewModel);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+
         public IActionResult Index()
         {
             return View();
