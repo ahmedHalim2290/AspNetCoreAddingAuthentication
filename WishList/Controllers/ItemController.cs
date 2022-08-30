ad480f9408e2847003d9dc08d07c2f5fa9b4c171
ahmed Abd El-Halim <a.halim2290@gmail.com>
2022-08-30 14:16:00 +0200

    .

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -53,15 +53,13 @@ namespace WishList.Controllers
 
         public IActionResult Delete(int id)
         {
-            var logedInUser = _userManager.GetUserAsync(HttpContext.User).Result;
-            if (logedInUser != null)
-            {
-                var item = _context.Items.FirstOrDefault(e => e.Id == id && e.User.Id == logedInUser.Id);
-                _context.Items.Remove(item);
-                _context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return Unauthorized();
+            var user = _userManager.GetUserAsync(HttpContext.User).Result;
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
+            if (item.User.Id != user.Id)
+                return Unauthorized();
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
